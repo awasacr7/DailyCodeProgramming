4656ee8d6c2bf2110b9c5a2e7c8cc15070b6ffc0
awasacr7 <yawanesh@gmail.com>
2019-08-15 20:49:09 +0900

    Solution for the program

---
+++ 15August2019.py
@@ -0,0 +1,15 @@
+def encoding(a):
+    if a.startswith('0'):
+        return 0
+    elif len(a)<=1:
+        return 1
+    
+    result=0
+    
+    if int(a[:2]) <= 26:
+        result += encoding(a[2:])
+    
+    result += encoding(a[1:])
+    return result
+
+print(encoding('602'))
